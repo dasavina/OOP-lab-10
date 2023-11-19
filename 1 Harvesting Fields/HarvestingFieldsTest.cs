e135669c9ddf20b8d2df39ebc706943c7f5f3d91
Savina Daria <savina.dariia@chnu.edu.ua>
2023-11-19 12:27:29 +0200

    1

--- 1 Harvesting Fields/HarvestingFieldsTest.cs
+++ 1 Harvesting Fields/HarvestingFieldsTest.cs
@@ -1,12 +1,64 @@
-﻿ namespace P01_HarvestingFields
+﻿namespace P01_HarvestingFields
 {
     using System;
+    using System.Reflection;
 
     public class HarvestingFieldsTest
     {
         public static void Main()
         {
-            //TODO put your reflection code here
+            Type type = typeof(HarvestingFields);
+            string command = Console.ReadLine();
+            while (!command.Equals("harvest"))
+            {
+                switch (command)
+                {
+                    case "public":
+                        {
+                            foreach (FieldInfo field in type.GetFields())
+                            {
+                                if (field.IsPublic)
+                                {
+                                    Console.WriteLine("public " + field.Name);
+                                }
+                            }
+                        }
+                        break;
+                    case "protected":
+                        {
+                            foreach (FieldInfo field in type.GetFields())
+                            {
+                                if (field.IsFamily)
+                                {
+                                    Console.WriteLine("protected " + field.Name);
+                                }
+                            }
+                        }
+                        break;
+                    case "private":
+                        {
+                            foreach (FieldInfo field in type.GetFields())
+                            {
+                                if (field.IsPrivate)
+                                {
+                                    Console.WriteLine("private " + field.Name);
+                                }
+                            }
+                        }
+                        break;
+                    case "all":
+                        {
+                            foreach (FieldInfo field in type.GetFields())
+                            {
+                                Console.WriteLine("public " + field.Name);
+                            }
+                        }
+                        break;
+                    default: { Console.WriteLine("no such command"); } break;
+                }
+           
+                command = Console.ReadLine();
+            }
         }
     }
 }
