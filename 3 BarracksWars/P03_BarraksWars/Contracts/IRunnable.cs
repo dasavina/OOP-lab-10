df3fb34fecf33b05cf0567af8ff37566aab335a3
Savina Daria <savina.dariia@chnu.edu.ua>
2023-11-20 16:36:21 +0200

    3?

--- 3 BarracksWars/P03_BarraksWars/Contracts/IRunnable.cs
+++ 3 BarracksWars/P03_BarraksWars/Contracts/IRunnable.cs
@@ -2,33 +2,6 @@
 {
     public interface IRunnable
     {
-        void Run() {
-            string command = Console.ReadLine();
-            while (!command.Equals("fight"))
-            {
-                string[] parameters = command.Split(' ');
-                switch (parameters[0])
-                {
-                    case "add":
-                        {
-
-
-                            Console.WriteLine("added");
-                        }
-                        break;
-                    case "report":
-                        {
-
-                        }
-                        break;
-                    default:
-                        {
-                            Console.WriteLine("no such command");
-                        }
-                        break;
-
-                }
-            }
-        }
+        void Run();
     }
 }
--- 3 BarracksWars/P03_BarraksWars/Core/Factories/UnitFactory.cs
+++ 3 BarracksWars/P03_BarraksWars/Core/Factories/UnitFactory.cs
@@ -1,14 +1,17 @@
 ﻿namespace _03BarracksFactory.Core.Factories
 {
     using System;
+    using System.Reflection;
+    using _03BarracksFactory.Models.Units;
     using Contracts;
 
     public class UnitFactory : IUnitFactory
     {
         public IUnit CreateUnit(string unitType)
         {
+            Type type = Assembly.GetExecutingAssembly().GetType(unitType);
+            return (IUnit)Activator.CreateInstance(type);
             //TODO: implement for Problem 3
-            throw new NotImplementedException();
         }
     }
 }
